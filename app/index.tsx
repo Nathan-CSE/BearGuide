f7e576c5227a335abd52d7aeb0a5c6bbf4a70ccd
Nathan-CSE <86046189+Nathan-CSE@users.noreply.github.com>
2024-11-01 14:48:39 +1100

    added nav bar

---
+++ app/index.tsx
@@ -0,0 +1,76 @@
+import * as React from 'react';
+import { AppRegistry } from 'react-native';
+import { MD3LightTheme as DefaultTheme, PaperProvider } from 'react-native-paper';
+import { BottomNavigation, Text } from 'react-native-paper';
+import { View, StyleSheet, Image, ScrollView } from "react-native";
+
+// This theme doesnt seem to fucking work
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'orange',
+    secondary: 'yellow',
+  },
+};
+
+const MapRoute = () => <Text>Map</Text>;
+
+const ProfileRoute = () => <Text>User Profile</Text>;
+
+const BottomNav = () => {
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'map', title: 'Map', focusedIcon: 'map'},
+    { key: 'profile', title: 'User Profile', focusedIcon: 'account-outline' }
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    map: MapRoute,
+    profile: ProfileRoute,
+  });
+
+  return (
+    <BottomNavigation
+      navigationState={{ index, routes }}
+      onIndexChange={setIndex}
+      renderScene={renderScene}
+      style={styles.container}
+    />
+  );
+
+}
+
+export default function Index() {
+  return (
+    <PaperProvider theme={theme}>
+      <BottomNav />
+    </PaperProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 50
+  },
+  baseText: {
+    fontFamily: 'Cochin',
+  },
+  titleText: {
+    fontWeight: 'bold',
+    margin: 'auto',
+    marginTop: 50
+  },
+  button: {
+    marginTop: 20,
+    marginHorizontal: 'auto'
+  },
+  textField: {
+    marginVertical: 10
+  },
+  createModal: {
+    borderWidth: 1,
+    padding: 20,
+    margin: 10
+  }
+});
