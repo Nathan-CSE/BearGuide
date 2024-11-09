1dcaae4d9be355c4eaa1d2a34403da9e25e9c1b9
Joon <joonhyoo12@gmail.com>
2024-11-09 18:51:50 +1100

    added tabbar for user profile

--- app/UserProfile.jsx
+++ app/UserProfile.jsx
@@ -1,27 +1,56 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
-import {
-  Avatar,
-  Button,
-  Surface,
-  Text,
-  Title,
-  Paragraph,
-} from 'react-native-paper';
+import { View, useWindowDimensions } from 'react-native';
+import { Avatar, Button, Surface, Text, useTheme } from 'react-native-paper';
+import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
 
 import { useBearGuide } from './BearGuideContext';
 import { StyleSheet } from 'react-native';
 
+const FirstRoute = () => (
+  <View style={{ flex: 1, backgroundColor: '#ff4081' }} />
+);
+
+const SecondRoute = () => (
+  <View style={{ flex: 1, backgroundColor: '#673ab7' }} />
+);
+
+const routes = [
+  { key: 'first', title: 'First' },
+  { key: 'second', title: 'Second' },
+];
+
 const UserProfile = () => {
+  const theme = useTheme();
   const { bearGuide, setBearGuide, tools } = useBearGuide();
   const [name, setName] = useState('Name');
   const [faculty, setFaculty] = useState('Faculty');
   const [campus, setCampus] = useState('Preferred campus');
-  const statusBarColor = '#6a51ae'; // Your desired color
+
+  const renderTabBar = (props) => (
+    <TabBar
+      {...props}
+      style={{
+        backgroundColor: theme.colors.elevation['level5'],
+      }}
+      activeColor={theme.colors.primary}
+      inactiveColor={theme.colors.onSurfaceVariant}
+      indicatorStyle={{
+        backgroundColor: theme.colors.primary,
+        height: 4,
+        width: 50,
+        borderTopStartRadius: 10,
+        borderTopEndRadius: 10,
+        left: (layout.width / 2 - 50) / 2,
+      }}
+    />
+  );
+
+  const layout = useWindowDimensions();
+  const [index, setIndex] = useState(0);
 
   return (
-    <View>
-      <Surface elevation={1} style={styles.headerContainer}>
+    <View style={styles.profileContainer}>
+      <Surface elevation={5} style={styles.headerContainer}>
         <View style={styles.userInfo}>
           <Avatar.Text size={80} label="NN" />
           <View>
@@ -37,17 +66,25 @@ const UserProfile = () => {
           </View>
           <Button mode="contained">Edit</Button>
         </View>
-        <Text>Reviews/Favourites</Text>
       </Surface>
-      <Text>
-        Reviews/Favourites section with component that contains two flatlists
-        which displays the corresponding list.
-      </Text>
+      <TabView
+        navigationState={{ index, routes }}
+        renderTabBar={renderTabBar}
+        renderScene={SceneMap({
+          first: FirstRoute,
+          second: SecondRoute,
+        })}
+        onIndexChange={setIndex}
+        initialLayout={{ width: layout.width }}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  profileContainer: {
+    flex: 1,
+  },
   headerContainer: {
     paddingHorizontal: 24,
     paddingVertical: 16,
