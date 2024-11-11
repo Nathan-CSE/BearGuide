513632b38f23682a317fe0ea4eb383211329578c
Nathan Nguyen <z5360538@ad.unsw.edu.au>
2024-11-11 21:23:54 +1100

    Update file BearGuideContext.jsx

--- app/BearGuideContext.jsx
+++ app/BearGuideContext.jsx
@@ -3,40 +3,15 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Define initial structure for bearGuide
 const initialBearGuide = {
+  currentUserId: null,
   locations: [
-    // {
-    //   id: 0,
-    //   coordinates: { long: 0, lat: 0 },
-    //   name: "",
-    //   reviews: {
-    //     summary: { accessibility: 0, cleanliness: 0, noisiness: 0, overall: 0 },
-    //     list: [
-    //       {
-    //         id: 0,
-    //         userId: 0,
-    //         accessibility: 0,
-    //         cleanliness: 0,
-    //         noisiness: 0,
-    //         overall: 0,
-    //         comment: ""
-    //       }
-    //     ],
-    //     amenities: [{ category: "", comment: "" }],
-    //     capacity: 0,
-    //     popularTimes: {
-    //       sunday: [], monday: [], tuesday: [], wednesday: [], thursday: [], friday: []
-    //     },
-    //     images: ["<IMAGE URI>"],
-    //     openingHours: { type: 0, data: {} },
-    //     description: ""
-    //   }
-    // }
     {
       id: 0,
       coordinates: { long: -33.918900523094244, lat: 151.23102394496718 },
+      address: "Science Rd, Kensington NSW 2033",
       name: "School of Computer Science and Engineering",
       reviews: {
-        summary: { accessibility: 0, cleanliness: 0, noisiness: 0, overall: 0 },
+        summary: { accessibility: 4.2, cleanliness: 4.2, noisiness: 4.2, overall: 4.2 },
         list: [
           {
             id: 0,
@@ -57,6 +32,53 @@ const initialBearGuide = {
         openingHours: { type: 1, data: {} },
         description: "Computer Science and Engineering"
       }
+    },
+    // Modified location object to include address string, popular times and opening hours
+    {
+      id: 1,
+      coordinates: { long: -33.918900523094244, lat: 151.23102394496718 },
+      address: "Science Rd, Kensington NSW 2033",
+      name: "Blockhouse G15",
+      reviews: {
+        summary: { accessibility: 4.2, cleanliness: 4.2, noisiness: 4.2, overall: 4.2 },
+        list: [
+          {
+            id: 0,
+            userId: 0,
+            accessibility: 5,
+            cleanliness: 5,
+            noisiness: 5,
+            overall: 5,
+            comment: "What an amazing building."
+          }
+        ],
+        amenities: [{ category: "power", comment: "Outlet Charging" }],
+        capacity: 50,
+        // example hourly levels from 12 AM to 11 PM
+        popularTimes: {
+          monday: [0, 0, 0, 0, 1, 2, 4, 6, 7, 6, 5, 4, 3, 2, 3, 5, 7, 6, 5, 4, 3, 2, 1, 0],
+          tuesday: [0, 0, 0, 0, 1, 2, 4, 5, 6, 6, 5, 4, 3, 3, 4, 5, 6, 7, 5, 4, 3, 2, 1, 0],
+          wednesday: [0, 0, 0, 0, 1, 3, 4, 6, 7, 6, 5, 5, 4, 3, 4, 5, 6, 6, 5, 3, 2, 1, 1, 0],
+          thursday: [0, 0, 0, 0, 1, 2, 3, 5, 6, 6, 5, 4, 3, 4, 5, 5, 6, 6, 5, 4, 3, 2, 1, 0],
+          friday: [0, 0, 0, 0, 1, 2, 4, 6, 8, 8, 7, 5, 5, 6, 7, 8, 7, 6, 5, 3, 2, 1, 0, 0],
+          saturday: [0, 0, 0, 1, 2, 3, 5, 6, 7, 8, 8, 7, 6, 5, 4, 5, 6, 5, 4, 3, 2, 1, 0, 0],
+          sunday: [0, 0, 0, 1, 2, 3, 4, 5, 5, 6, 5, 4, 3, 4, 5, 5, 4, 4, 3, 2, 1, 0, 0, 0]
+        },
+        images: ["https://s3.studentvip.com.au/photos/blockhouse-g6-opposite-tyree-bui-174303-small.jpg"],
+        openingHours: {
+          type: 1,  // assuming 1 indicates regular weekly hours
+          data: {
+            monday: { open: "9:00 AM", close: "5:00 PM" },
+            tuesday: { open: "9:00 AM", close: "5:00 PM" },
+            wednesday: { open: "9:00 AM", close: "5:00 PM" },
+            thursday: { open: "9:00 AM", close: "5:00 PM" },
+            friday: { open: "9:00 AM", close: "5:00 PM" },
+            saturday: { open: "10:00 AM", close: "3:00 PM" },
+            sunday: { open: "Closed", close: "Closed" }
+          }
+        },
+        description: "The Blockhouse is home to several shops including Thoughtful Foods, Medibank and the UNSW Secondhand Book store and various rooms. Foundation studies take place inside the Blockhouse on levels 1 and 2 which is accessible from the back entrance near Roundhouse."
+      }
     }
   ],
   users: [
@@ -68,8 +90,19 @@ const initialBearGuide = {
       faculty: "Computer Science and Engineering",
       campus: "UNSW Kensington",
       reviews: [{ locationId: 0, reviewId: 0 }],
-      favourites: [0],
-      recents: [0]
+      favourites: [0], // Array of integers, integers represents locationId
+      recents: [0] // Possibly related to search?
+    },
+    {
+      id: 1,
+      name: "Jane Doe",
+      email: "janedoe@example.com",
+      password: "bearguide",
+      faculty: "Computer Science and Engineering",
+      campus: "UNSW Kensington",
+      reviews: [{ locationId: 1, reviewId: 0 }],
+      favourites: [1], // Array of integers, integers represents locationId
+      recents: [1]
     }
   ]
 };
