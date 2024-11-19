a5d56d6046e3120a2844921c3f7a2b0141b32495
Joon <joonhyoo12@gmail.com>
2024-11-19 19:42:42 +1100

    created landing page

--- app/index.jsx
+++ app/index.jsx
@@ -1,18 +1,77 @@
-import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
 import React from 'react';
 import { Link } from 'expo-router';
-import { Button } from 'react-native-paper';
+import { Button, Text, useTheme } from 'react-native-paper';
 import a from '@/assets/images/Expanded Logo.png';
 
 const index = () => {
+  const theme = useTheme();
+
   return (
-    <SafeAreaView>
-      <Image source={a} style={{ maxWidth: '100%', height: '100%' }} />
-      <Text>index</Text>
-      <Link href={'/home'}>
-        <Button>Go to Home</Button>
-      </Link>
-    </SafeAreaView>
+    <View style={{ flex: 1 }}>
+      <Image
+        source={a}
+        style={{
+          resizeMode: 'contain',
+          maxHeight: '100%',
+          maxWidth: '100%',
+        }}
+      />
+      <View
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          width: '100%',
+          height: '50%',
+          paddingVertical: 48,
+          paddingHorizontal: 48,
+          gap: 32,
+          backgroundColor: 'white',
+          alignItems: 'center',
+          padding: 20,
+          borderTopEndRadius: 50,
+          borderTopStartRadius: 50,
+        }}
+      >
+        <View style={{ alignItems: 'flex-start' }}>
+          <Text
+            variant="displayMedium"
+            style={{ fontWeight: 'bold', color: theme.colors.onTertiary }}
+          >
+            Welcome to
+          </Text>
+          <Text
+            variant="displayLarge"
+            style={{ fontWeight: 'bold', color: theme.colors.primaryContainer }}
+          >
+            BearGuide
+          </Text>
+        </View>
+        <View style={{ width: '100%', gap: 16 }}>
+          <Button
+            mode="contained"
+            onPress={() => console.log('go to starting guide')}
+          >
+            Get Started
+          </Button>
+          <Link href={'/LoginPage'}>
+            <View
+              style={{
+                width: '100%',
+                alignItems: 'center',
+              }}
+            >
+              <Text
+                variant="bodyLarge"
+                style={{ fontWeight: 'bold', textDecorationLine: 'underline' }}
+              >
+                Skip to Login/Register
+              </Text>
+            </View>
+          </Link>
+        </View>
+      </View>
+    </View>
   );
 };
 
