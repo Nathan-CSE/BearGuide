ef8c407dbe3248bf9c9330ac1e28fbbf8b963faa
Xander Gonzaga <x.gonzaga@student.unsw.edu.au>
2024-11-19 21:21:08 +1100

    Update _layout.jsx

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -42,8 +42,10 @@ export default function RootLayout() {
           />
         </Stack>
         <StatusBar 
+          style="dark"
           translucent={true}
           hidden={false}
+          
         />
       </PaperProvider>
     </BearGuideProvider>
